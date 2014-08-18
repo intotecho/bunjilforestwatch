5b2e5f7f1fddd73f28560e666d42c3f6972bbd6b
Chris Goodman <bunjilforestwatch@gmail.com>
2014-08-18 11:42:52 +1000

    refactores fetchOVerlay to take an overlay name and a tooltip instead of an obs. improved logging of jobid's Fixed indexOf() exception when no "@" present in overlay_name.

--- static/js/overlay-mgr.js
+++ static/js/overlay-mgr.js
@@ -10,23 +10,13 @@ function createLandsatGridOverlay(map, opacity, clickable, cellarray) {
     if (map.landsatGridOverlay === undefined){
         var landsatGridOverlay = new LandsatGridOverlay(map, opacity, clickable, cellarray);
         landsatGridOverlay.name = "grid" ;
-        landsatGridOverlay.initialize();
-        
+        landsatGridOverlay.initialize();       
         overlayMaps.push(landsatGridOverlay);
-        /*
-        addLayer( landsatGridOverlay.name,
-                  'Landsat Cell Border',
-                  'gray',  
-                  100 * opacity, 
-                  "Each Landsat image covers one of these cells.", 
-                  layerslider_callback ); //create slider.
-        */
+       
         map['landsatGridOverlay'] = landsatGridOverlay;
     }
 }
    
-
-
 //find overlay mathching role and viz algorithm.
 function findOverlay(obs, role, algorithm) {
     for (o = 0; o < obs.overlays.length; o++) {
@@ -38,67 +28,39 @@ function findOverlay(obs, role, algorithm) {
     return null;
 }
 
-
 function requestAdHocOverlay() { //not assoc with a task...
  	var httpget_url = httpgetActionUrl("overlay")
     console.log( "urls", httpget_url);
+ 	var overlayname = "AdHoc overlay";
+    var tooltip = "AdHoc overlay - " + httpget_url;
 	
-	fetchOverlay(null, "prompt for new overlay", httpget_url );
-	
+    fetchOverlay(overlayname, tooltip, "<small>Creating new AdHoc overlay</small>", httpget_url );
 }
 
 
-function displayOverlay(ovl, overlayname, tooltip) { //overlay is current so add it to the map.
-    
-    var map =    ((ovl.overlay_role == 'latest') ?  map_lhs : map_rhs);    
-    var colour; 
-    
-    if (ovl.overlay_role == 'latest') 
-    	colour = 'red';
-    else if (ovl.overlay_role == 'prior') 
-    	colour = 'green'  
-    else 
-    	colour = 'blue';
-    
-    //addJob("<p class = 'small'>" + ovl.reason +"</p><br>")
-
-    createImageOverlay(true, map, ovl.map_id,  ovl.token, overlayname, tooltip, colour);
-}
-
-function fetchOverlay(obs, prompt, httpget_url) {	
-      prompt += '<img src="/static/img/ajax-loader.gif" class="ajax-loader"/>';
-      jobid = addJob(prompt, 'black');
+function fetchOverlay(overlayname, tooltip, prompt, httpget_url) {	
+	  
+	  prompt += ' <img src="/static/img/ajax-loader.gif" class="ajax-loader"/>';
+      
+	  var jobid = addJob(prompt, 'green');
       
       $.get(httpget_url).done(function(data) {
-	       var tooltip;
-	       var overlayname;
 	       var ovl = jQuery.parseJSON(data);
-	   
 	       if (ovl) {
-	    	   if (obs != null ) {
-		    	   	tooltip = "Overlay: " +  obs.captured  +  " Id: " + obs.image_id + " " + ovl.overlay_role + " " + ovl.algorithm;
-		            overlayname = obs.captured + ":" + ovl.overlay_role;          
-		       }
-		       else {
-		    	   	tooltip = "AdHoc overlay";
-		            overlayname = tooltip;        
-		       }
-		       if (ovl.result =='error')
-		    	   {
+		       if (ovl.result =='error') {
 	    	   		updateJob(jobid, "<p class = 'small'>" + ovl.reason +"</p><br>", 'red');
 		    	   }
 		       else {
-		    	   removeJob(jobid);
 		    	   displayOverlay(ovl, overlayname, tooltip);
+		    	   removeJob(jobid);
 		       }
 	       }
 	       else {
 	    	   		updateJob(jobid, "<p class = 'small'>No Overlay Data</p><br>", 'red');
-
 	       }
 	  }).error(function( jqxhr, textStatus, error ) {
 	           var err = textStatus + ', ' + error;
-	           console.log( "Request Failed: " + err);
+	           console.log( "Request " + jobid + " Failed: " + err);
 	           updateJob(jobid, "<p class='small'>Failed: " + err + "</p><br>", 'red');
 	  });
 }
@@ -110,7 +72,8 @@ function updateOverlay(ovl, overlayname, tooltip) {
 	var httpget_url = 'overlay/update/' + ovl.key + '/' + ovl.algorithm; 
     
     console.log( "updateOverlay() %s as: %s", prompt, httpget_url);
-    fetchOverlay(obs,'<small>' + prompt + '</small>', httpget_url );
+    
+    fetchOverlay(overlayname, tooltip, '<small>' + prompt + '</small>', httpget_url );
 }
 
 function createObsOverlay(obs, role, algorithm) {
@@ -121,7 +84,10 @@ function createObsOverlay(obs, role, algorithm) {
        
     console.log( "createObsOverlay() %s as: %s", prompt, httpget_url);
     
-    fetchOverlay(obs, '<small>' + prompt + '</small>', httpget_url);
+    var overlayname = obs.captured;          
+    var tooltip = "Overlay: " +  overlayname + " " + role  + " " +  algorithm + " Image: " + obs.image_id ;
+    
+    fetchOverlay(overlayname, tooltip, '<small>' + prompt + '</small>', httpget_url);
 }
 
 
@@ -150,7 +116,22 @@ function displayObsOverlay(obs, role, algorithm) {
         createObsOverlay(obs, role, algorithm);
     }
 }    
- 
+
+function displayOverlay(ovl, overlayname, tooltip) { //overlay is current so add it to the map.
+    
+    var map =    ((ovl.overlay_role == 'latest') ?  map_lhs : map_rhs);    
+    var colour; 
+    
+    if (ovl.overlay_role == 'latest') 
+    	colour = 'red';
+    else if (ovl.overlay_role == 'prior') 
+    	colour = 'green'  
+    else 
+    	colour = 'blue';
+
+    createImageOverlay(true, map, ovl.map_id,  ovl.token, overlayname, tooltip, colour);
+}
+
  
 /*      
  * createImageOverlay(true, map_lhs, mapobj.mapid,  mapobj.token, mapobj.date_acquired, tooltip, "cyan");
@@ -160,9 +141,14 @@ function createImageOverlay(show, google_map, map_id,  token, overlay_name, tool
 {
     if(show == true)
     {
-        var shortname = overlay_name.substr(0, overlay_name.indexOf("@")); //@ is invalid in DIV tag id.
-            
-        var count = 0;
+        var idx = overlay_name.indexOf("@");
+        
+    	if (idx == -1) {
+    		idx = 30; // maxlen
+    	}
+		var shortname = overlay_name.substr(0, idx); //@ is invalid in DIV tag id.
+
+    	var count = 0;
         while(findOverlayLayer(shortname + count.toString(), overlayMaps) != -1) {
         	count++;
             console.log("createImageOverlay incrementing index: " + overlay_name );
@@ -195,7 +181,7 @@ function createImageOverlay(show, google_map, map_id,  token, overlay_name, tool
 
         
         google_map.overlayMapTypes.push(null); //  Placeholder for layer
-        z  = google_map.overlayMapTypes.length-1;
+        var z  = google_map.overlayMapTypes.length-1;
         overlay.index = z;
         google_map.overlayMapTypes.insertAt(z, overlay);
         
@@ -212,10 +198,6 @@ function createImageOverlay(show, google_map, map_id,  token, overlay_name, tool
 
 
 function findOverlayLayer(layer_id, overlayMaps){
-	//    return $.grep(overlayMaps, function(obj){
-	//        return obj.name == layer_id;
-	//  });
-
     for (var i=0; i < overlayMaps.length; i++) {
         if (overlayMaps[i].name == layer_id)
             return i;
@@ -253,8 +235,9 @@ function removeFromMap(map, overlay_id)
 function removeJob(job_id)
 {
   var panel = $("#jobs_table");
-    console.log("panel", panel);
-    jobid = "#" + job_id;
+    //console.log("panel", panel);
+    console.log("removeJob id:", job_id);
+    var jobid = "#" + job_id;
     var job = panel.find(jobid);
     job.fadeOut(800);
 }
@@ -262,9 +245,9 @@ function removeJob(job_id)
 function updateJob(job_id, text, colour)
 {
     var panel = $("#jobs_table");
-    jobid = "#" + job_id;
+    var jobid = "#" + job_id;
+    console.log("updateJob, id:", jobid);
     var job = panel.find(jobid);
-    console.log("job", job);
     
     var label = job.find("#tlabel_id");
     console.log("label", label);
@@ -283,7 +266,7 @@ function addJob(text, colour)
     	return 0;
     }
     // This id should be different for each instance.
-    job_id = "job_id" + (numjobs++).toString();
+    var job_id = "job_id" + (numjobs++).toString();
         
     newDiv.attr("id", job_id);
     newDiv.id=job_id;
@@ -302,5 +285,6 @@ function addJob(text, colour)
     
     newDiv.show();
     $("#jobs_table").append(newDiv);
+    console.log("addJob id:" +  job_id + " " + text);
     return job_id;
 }
