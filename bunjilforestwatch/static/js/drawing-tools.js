64e0dca59e9324e18ef5bb3603fa4c464d2a1f6d
Chris Goodman <bunjilforestwatch@gmail.com>
2015-08-25 18:12:22 +1000

    Fixed Dragging to work on touch devices - tested on iPad.
    Replaced plugin from jquery 'draggable-object.js' with
    jquery.udraggable.js. This relies on jquery.event.ue.js which harmonises
    touch and mouse events.
    Also updated the drag handle icon with a new dragger.png and adjusted
    the labels. 
    Google Map Drawing tools now defaults to a place mark and has less
    options.

--- bunjilforestwatch/static/js/drawing-tools.js
+++ bunjilforestwatch/static/js/drawing-tools.js
@@ -5,16 +5,16 @@ Licences: Creative Commons Attribution
 
 function createDrawingManager(map) {
 	var drawingManager = new google.maps.drawing.DrawingManager({
-	    drawingMode: google.maps.drawing.OverlayType.POLYGON,
+	    drawingMode: google.maps.drawing.OverlayType.MARKER,
 	    drawingControl: true,
 	    drawingControlOptions: {
 	      position: google.maps.ControlPosition.TOP_CENTER,
 	      drawingModes: [
 	        google.maps.drawing.OverlayType.MARKER,
-	        google.maps.drawing.OverlayType.CIRCLE,
+	        //google.maps.drawing.OverlayType.CIRCLE,
 	        google.maps.drawing.OverlayType.POLYGON,
-	        google.maps.drawing.OverlayType.POLYLINE,
-	        google.maps.drawing.OverlayType.RECTANGLE
+	        //google.maps.drawing.OverlayType.POLYLINE,
+	        //google.maps.drawing.OverlayType.RECTANGLE
 	      ]
 	    },
 	    markerOptions: {
