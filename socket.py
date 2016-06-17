d102339c9f2b3cbc55b2e9bfdacfc6d5392879ba
Chris Goodman <bunjilforestwatch@gmail.com>
2016-06-17 14:25:11 +1000

    updates the patch file socket.py project runs after rebuilding the library on the latest python 2.7.11

--- socket.py
+++ socket.py
@@ -70,7 +70,6 @@ else:
 
     # we need to import the same constants we used to...
     from _ssl import SSLError as sslerror
-    from _ssl import RAND_add, RAND_egd, RAND_status, SSL_ERROR_ZERO_RETURN, SSL_ERROR_WANT_READ, SSL_ERROR_WANT_WRITE, SSL_ERROR_WANT_X509_LOOKUP, SSL_ERROR_SYSCALL, SSL_ERROR_SSL, SSL_ERROR_WANT_CONNECT, SSL_ERROR_EOF, SSL_ERROR_INVALID_ERROR_CODE
     from _ssl import \
         RAND_add, \
         RAND_status, \
