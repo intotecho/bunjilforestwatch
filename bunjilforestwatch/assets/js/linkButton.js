27aa3acc75f7f64116548fb3d235511559fd2fc5
Zakariah Chitty <zakariah.chitty@papercut.com>
2016-08-30 21:23:41 +1000

    SFW-19: Fixed issue with the link button that caused page load failures on the observation task screen.

--- bunjilforestwatch/assets/js/linkButton.js
+++ bunjilforestwatch/assets/js/linkButton.js
@@ -4,13 +4,13 @@ import classNames from 'classnames';
 
 export default React.createClass({
   render() {
-  	let classNames({
-  		`LinkButton`: true,
-  		 `${this.props.classNames}`: !!this.props.classNames
+  	let linkButtonClasses = classNames({
+  		'LinkButton': true,
+  		 [`${this.props.classNames}`]: !!this.props.classNames
   	});
 
     return (
-      <a className={classNames} href={this.props.link}>
+      <a className={linkButtonClasses} href={this.props.link}>
       	{this.props.name}
       </a>
     );
