7e009a4b8bbc6d6201cacdec958c3d2428f2c4b4
Sizuki <monkeyintomischief@hotmail.com>
2016-08-30 18:54:04 +1000

    Add classnames module to turn CSS on conditionally

--- bunjilforestwatch/assets/js/linkButton.js
+++ bunjilforestwatch/assets/js/linkButton.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
+import classNames from 'classnames';
+
 export default React.createClass({
   render() {
-  	let classNames = `LinkButton ${this.props.classNames}`;
+  	let classNames({
+  		`LinkButton`: true,
+  		 `${this.props.classNames}`: !!this.props.classNames
+  	});
 
     return (
       <a className={classNames} href={this.props.link}>
