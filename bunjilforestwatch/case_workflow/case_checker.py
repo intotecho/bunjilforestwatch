9ffaaf76be7a204a087e9df287a174a10b161175
NathanDamilatis <nathan.damilatis@gmail.com>
2016-09-13 14:16:22 +1000

    SFW-28: Resolved issues with CaseVotes being uniterable, cleaned up CaseChecker object definition, cases now close when a majority has been reached

--- bunjilforestwatch/case_workflow/case_checker.py
+++ bunjilforestwatch/case_workflow/case_checker.py
@@ -1,37 +1,41 @@
-from models import VOTE_CATEGORIES
+from models import VOTE_CATEGORIES, Case, CaseVotes
 
 
 class CaseChecker(object):
     MIN_VOTES_FOR_VIABLE_CONSENSUS = 6
     MIN_CONSENSUS = 66
 
-    @staticmethod
     def has_a_majority(self, case):
         """
         :return: True: if the votes for a case form a clear majority for one vote category. Based on MIN_CONSENSUS
         and MIN_VOTES_FOR_VIABLE_CONSENSUS requirements.
         """
         highest_category_votes = 0
-        total_votes = 0
-        for category_votes in case.votes:
-            total_votes += category_votes
-            if category_votes > highest_category_votes:
-                highest_category_votes = category_votes
+        total_votes = (case.votes.fire + case.votes.agriculture + case.votes.deforestation + case.votes.road)
+        if case.votes.fire > highest_category_votes:
+            highest_category_votes = case.votes.fire
+        if case.votes.agriculture > highest_category_votes:
+            highest_category_votes = case.votes.agriculture
+        if case.votes.deforestation > highest_category_votes:
+            highest_category_votes = case.votes.deforestation
+        if case.votes.road > highest_category_votes:
+            highest_category_votes = case.votes.road
+        if case.votes.unsure == highest_category_votes:
+            highest_category_votes = case.votes.unsure
+
         if highest_category_votes > 0:
             highest_category_votes = (highest_category_votes / total_votes) * 100
         if highest_category_votes > self.MIN_CONSENSUS:
             return True
         return False
 
-    @staticmethod
     def is_min_votes(self, case):
         """
         :return: True if the case has the minimum number of votes for the consensus (or lack of consensus) to be
         considered viable for actioning (notifying local subscribers). False if more votes are required (e.g., a
         consensus of one vote is not very trustworthy).
         """
-        case_votes = case.votes
-        total_votes = (case_votes.fire + case_votes.agriculture + case_votes.deforestation + case_votes.road)
+        total_votes = (case.votes.fire + case.votes.agriculture + case.votes.deforestation + case.votes.road)
         if total_votes >= self.MIN_VOTES_FOR_VIABLE_CONSENSUS:
             return True
         return False
@@ -42,23 +46,27 @@ class CaseChecker(object):
         Returns the VOTE_CATEGORY that corresponds with the highest vote
         A possible bug is 50/50 splits
         """
-        case_votes = case.votes
         highest_category_votes = 0
-        for category in case_votes:
-            if category > highest_category_votes:
-                    highest_category_votes = category
+
+        if case.votes.fire > highest_category_votes:
+            highest_category_votes = case.votes.fire
+        if case.votes.agriculture > highest_category_votes:
+            highest_category_votes = case.votes.agriculture
+        if case.votes.deforestation > highest_category_votes:
+            highest_category_votes = case.votes.deforestation
+        if case.votes.road > highest_category_votes:
+            highest_category_votes = case.votes.road
+
         if highest_category_votes == 0:
             return None
-        elif case_votes.fire == highest_category_votes:
-            return VOTE_CATEGORIES.FIRE
-        elif case_votes.agriculture == highest_category_votes:
-            return VOTE_CATEGORIES.AGRICULTURE
-        elif case_votes.deforestation == highest_category_votes:
-            return VOTE_CATEGORIES.DEFORESTATION
-        elif case_votes.road == highest_category_votes:
-            return VOTE_CATEGORIES.ROAD
-        elif case_votes.unsure == highest_category_votes:
-            return VOTE_CATEGORIES.UNSURE
+        elif case.votes.fire == highest_category_votes:
+            return CaseVotes.fire
+        elif case.votes.agriculture == highest_category_votes:
+            return CaseVotes.agriculture
+        elif case.votes.deforestation == highest_category_votes:
+            return CaseVotes.deforestation
+        elif case.votes.road == highest_category_votes:
+            return CaseVotes.road
 
 
 def total_votes(self, case_votes):
--- bunjilforestwatch/case_workflow/case_workflow_manager.py
+++ bunjilforestwatch/case_workflow/case_workflow_manager.py
@@ -3,7 +3,7 @@
 import case_checker
 import models
 
-checker = case_checker.CaseChecker
+checker = case_checker.CaseChecker()
 Case = models.Case
 
 
@@ -11,31 +11,30 @@ class CaseWorkflowManager(object):
     def __init__(self):
         pass
 
-    @staticmethod
     def update_case(self, case):
         """Do some stuff"""
         self.close_case(case, checker.get_most_voted_category(case))
 
-    @staticmethod
     def close_case(self, case, majority_held):
         """Do some stuff"""
-        case.status = 'CLOSED'
+        case.status = "CLOSED"
+        case.put()
 
     def check_cases(self):
         open_cases_query = Case.query(Case.status == 'OPEN')
         for open_case in open_cases_query:
             update_case_state = False
             if open_case is not None:
-                if checker.is_min_votes(checker, open_case):
-                    if checker.has_a_majority(checker, open_case):
+                if checker.is_min_votes(open_case):
+                    if checker.has_a_majority(open_case):
                         update_case_state = True
                 if update_case_state:
                     self.update_case(open_case)
 
     def check_single_case(self, case):
         update_case_state = False
-        if checker.is_min_votes(checker, case):
-            if checker.has_a_majority(checker, case):
+        if checker.is_min_votes(case):
+            if checker.has_a_majority(case):
                 update_case_state = True
         if update_case_state:
             self.update_case(case)
