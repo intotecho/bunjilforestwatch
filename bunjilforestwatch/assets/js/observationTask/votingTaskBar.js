e4416d3ed5108c03023dcdfdb61fa2ec2cd8940a
Sizuki <monkeyintomischief@hotmail.com>
2016-09-14 15:47:20 +1000

    SFW-32: Formatting on votingTaskBar

--- bunjilforestwatch/assets/js/observationTask/votingTaskBar.js
+++ bunjilforestwatch/assets/js/observationTask/votingTaskBar.js
@@ -20,54 +20,56 @@ const categoryImages = {
 }
 
 export default React.createClass({
-	votingHandler({ target: { innerText } }) {
-		// Should output or provide visual cue that an error has occurred
-		if (!CATEGORIES.includes(innerText) || !this.props.caseId) { return; }
+  votingHandler({ target: { innerText } }) {
+    // Should output or provide visual cue that an error has occurred
+    if (!CATEGORIES.includes(innerText) || !this.props.caseId) { return; }
 
-		let self = this;
-		let payload = {
-			case_id: this.props.caseId,
-			vote_category: innerText.toUpperCase()
-		};
+    let self = this;
+    let payload = {
+      case_id: this.props.caseId,
+      vote_category: innerText.toUpperCase()
+    };
 
-		Request
-		.post('/observation-task/response')
-		.send(payload)
-		.set('Accept', 'application/json')
-	  .end(
-	  	function(err, res) {
+    Request
+    .post('/observation-task/response')
+    .send(payload)
+    .set('Accept', 'application/json')
+    .end(
+      function(err, res) {
         // Interminently fails here, placing a log to capture the issue
         console.log(err);
         console.log(res);
 
-	  		// Should output or provide visual cue that an error has occurred
-	  		if (err == null && res.ok) {
-					self.props.setNextTask();
-	  		}
-	  	}
-	  );
-	},
+        // Should output or provide visual cue that an error has occurred
+        if (err == null && res.ok) {
+          self.props.setNextTask();
+        }
+      }
+    );
+  },
 
-	renderCategoryList() {
-		let categoryList = CATEGORIES.map((category, index) => {
-			return 	<li key={index} className={categoryListItem} onClick={this.votingHandler}>
-								<Button classNames={categoryButton}>
-                  <Icon classNames={categoryIcon} src={categoryImages[category]} />
-                  {category}
-                </Button>
-							</li>;
-		});
+  renderCategoryList() {
+    let categoryList = CATEGORIES.map((category, index) => {
+      return  (
+        <li key={index} className={categoryListItem} onClick={this.votingHandler}>
+          <Button classNames={categoryButton}>
+            <Icon classNames={categoryIcon} src={categoryImages[category]} />
+            {category}
+          </Button>
+        </li>
+      );
+    });
 
-		return <ul className={categoryOptionList}>{categoryList}</ul>;
-	},
+    return <ul className={categoryOptionList}>{categoryList}</ul>;
+  },
 
   render() {
     const titleClasses = `${title} ${uTextAlignCenter}`;
 
     return (
       <div className={container}>
-      	<p className={titleClasses}>Category</p>
-      	{this.renderCategoryList()}
+        <p className={titleClasses}>Category</p>
+        {this.renderCategoryList()}
       </div>
     );
   }
