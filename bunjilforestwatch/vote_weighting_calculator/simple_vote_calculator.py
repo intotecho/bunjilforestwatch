86ae506c8a472771ff396c99e0af9ec83e644b1b
Sizuki <monkeyintomischief@hotmail.com>
2016-09-16 14:29:44 +1000

    SFW-34: Change class reference to self

--- bunjilforestwatch/vote_weighting_calculator/simple_vote_calculator.py
+++ bunjilforestwatch/vote_weighting_calculator/simple_vote_calculator.py
@@ -5,15 +5,15 @@ class SimpleVoteCalculator(object):
   def get_weighted_vote(self, user, case, time_spent=0.0):
     # IF trust is greater than threshold number THEN
     # We do not consider time_spent in weighting
-    if user.trust > SimpleVoteCalculator.USER_TRUST_THRESHOLD:
+    if user.trust > self.USER_TRUST_THRESHOLD:
       return user.trust
     else:
-      if time_spent > SimpleVoteCalculator.TIME_SPENT_THRESHOLD:
-        time_spent = SimpleVoteCalculator.TIME_SPENT_THRESHOLD
+      if time_spent > self.TIME_SPENT_THRESHOLD:
+        time_spent = self.TIME_SPENT_THRESHOLD
       elif time_spent == 0:
         # Prevent division by zero
         time_spent = 0.01
 
       # Return user's trust value based on the amount of time_spent
       # Each second provides a full 20% value of trust
-      return user.trust * (time_spent / SimpleVoteCalculator.TIME_SPENT_THRESHOLD)
+      return user.trust * (time_spent / self.TIME_SPENT_THRESHOLD)
