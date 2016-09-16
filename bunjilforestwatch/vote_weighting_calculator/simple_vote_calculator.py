3e62e9d52a6c7339324cc59d60cc82771461456c
Zakariah Chitty <zakariah.chitty@papercut.com>
2016-09-16 16:45:23 +1000

    SFW-34: Fixed indentation issues.

--- bunjilforestwatch/vote_weighting_calculator/simple_vote_calculator.py
+++ bunjilforestwatch/vote_weighting_calculator/simple_vote_calculator.py
@@ -2,35 +2,36 @@ from user_trust.user_trust_manager import UserTrustManager
 
 user_trust_manager = UserTrustManager()
 
+
 class SimpleVoteCalculator(object):
-  """
-  Represents the number of seconds that has to pass in order for the user's
-  maximum trust value to be realized
-  """
-  TASK_DURATION_SECONDS_THRESHOLD = 5.0
-
-  def get_weighted_vote(self, user, case, task_duration_seconds=0.01):
     """
-    IF trust is greater than threshold number THEN
-    We do not consider task_duration_seconds in weighting
+    Represents the number of seconds that has to pass in order for the user's
+    maximum trust value to be realized
     """
-    if user.trust > self.get_user_trust_threshold():
-      return user.trust
-    else:
-      if task_duration_seconds > self.TASK_DURATION_SECONDS_THRESHOLD:
-        task_duration_seconds = self.TASK_DURATION_SECONDS_THRESHOLD
+    TASK_DURATION_SECONDS_THRESHOLD = 5.0
 
-      """
-      Return user's trust value based on the amount of task_duration_seconds
-      Each second provides a full 20% value of trust
-      """
-      return self.get_task_duration_vote_modifier(user.trust, task_duration_seconds)
+    def get_weighted_vote(self, user, case, task_duration_seconds=0.01):
+        """
+        IF trust is greater than threshold number THEN
+        We do not consider task_duration_seconds in weighting
+        """
+        if user.trust > self.get_user_trust_threshold():
+            return user.trust
+        else:
+            if task_duration_seconds > self.TASK_DURATION_SECONDS_THRESHOLD:
+                task_duration_seconds = self.TASK_DURATION_SECONDS_THRESHOLD
 
-  def get_task_duration_vote_modifier(self, trust_value, task_duration_seconds):
-    return trust_value * (task_duration_seconds / self.TASK_DURATION_SECONDS_THRESHOLD)
+            """
+            Return user's trust value based on the amount of task_duration_seconds
+            Each second provides a full 20% value of trust
+            """
+            return self.get_task_duration_vote_modifier(user.trust, task_duration_seconds)
 
-  def get_user_trust_threshold(self):
-    """
-    :return: The amount of trust that is gained or lost when the user responses correctly or incorrectly.
-    """
-    return user_trust_manager.get_max_trust() * 0.2
+    def get_task_duration_vote_modifier(self, trust_value, task_duration_seconds):
+        return trust_value * (task_duration_seconds / self.TASK_DURATION_SECONDS_THRESHOLD)
+
+    def get_user_trust_threshold(self):
+        """
+        :return: The amount of trust that is gained or lost when the user responses correctly or incorrectly.
+        """
+        return user_trust_manager.get_max_trust() * 0.2
